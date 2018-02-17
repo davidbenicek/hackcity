a7745a47cfac7d4a6fb8a47e59fe78c752b9b3a9
Sebastian Köhler <sebastiankoehler2@gmx.net>
2018-02-17 22:02:09 +0000

    added Balance

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,35 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { HTTP } from '@ionic-native/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  
-  constructor(public navCtrl: NavController, public http: HTTP) {
+  euros = 0;
+  cents = 0;
 
+  user = 'malcolm';
+  apiUrl = 'http://localhost:1200';
+  balanceUrl = this.apiUrl + 'balance?user=';
 
+  constructor(public navCtrl: NavController, public http:HttpClient) {
+    var json = this.getBalance()
+  }
 
-    this.http.get('http://ionic.io', {}, {})
-    .then(data => {
-  
-      console.log(data.status);
-      console.log(data.data); // data received by server
-      console.log(data.headers);
-  
-    })
-    .catch(error => {
-  
-      console.log(error.status);
-      console.log(error.error); // error message as string
-      console.log(error.headers);
-  
+  getBalance() {
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl+ this.user).subscribe(data => {
+        resolve(data);
+        console.log(data);
+      }, err => {
+        console.log(err);
+      });
     });
-
-
   }
 
 }
