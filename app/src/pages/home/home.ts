f5b2ee3ae47223747057499133a999ab1ecdddce
Sebastian Köhler <sebastiankoehler2@gmx.net>
2018-02-17 20:07:40 +0000

    added images

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HTTP } from '@ionic-native/http';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  
+  constructor(public navCtrl: NavController, public http: HTTP) {
+
+
+
+    this.http.get('http://ionic.io', {}, {})
+    .then(data => {
+  
+      console.log(data.status);
+      console.log(data.data); // data received by server
+      console.log(data.headers);
+  
+    })
+    .catch(error => {
+  
+      console.log(error.status);
+      console.log(error.error); // error message as string
+      console.log(error.headers);
+  
+    });
+
 
   }
 
