1fe9da51b79908e6f693ae54f6b573b66f329958
Sebastian Köhler <sebastiankoehler2@gmx.net>
2018-02-17 22:34:54 +0000

    added response handling

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -14,17 +14,23 @@ export class HomePage {
 
   user = 'malcolm';
   apiUrl = 'http://localhost:1200';
-  balanceUrl = this.apiUrl + 'balance?user=';
+  balanceUrl = this.apiUrl + '/balance?user=';
 
   constructor(public navCtrl: NavController, public http:HttpClient) {
     var json = this.getBalance()
+    json.then( x => {
+      console.log(x.amount);
+      var amount = (x.amount).toString();
+      this.euros = amount.split('.')[0]
+      this.cents = amount.split('.')[1]
+    } ) 
   }
 
   getBalance() {
     return new Promise(resolve => {
-      this.http.get(this.apiUrl+ this.user).subscribe(data => {
+      this.http.get('http://localhost:1200/balance').subscribe(data => {
+        //console.log(data);
         resolve(data);
-        console.log(data);
       }, err => {
         console.log(err);
       });
