c290c45863332babcfa2fb1fa7d72b99215b6dd2
Sebastian Köhler <sebastiankoehler2@gmx.net>
2018-02-18 08:09:07 +0000

    fixed inconsistency

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -9,34 +9,41 @@ import { HttpClient } from '@angular/common/http';
 
 export class HomePage {
 
-  hideMenu = false;
+  hideMenu = true;
 
-  euros = 1234;
-  cents = 45;
+  euros = "1234";
+  cents = "45";
 
   user = 'malcolm';
   apiUrl = 'http://localhost:1200';
   balanceUrl = this.apiUrl + '/balance?user=';
 
   constructor(public navCtrl: NavController, public http:HttpClient) {
+    interface Balance {
+      amount:    string;
+      effectiveBalance:     string;
+      availableToSpend: string;
+      pendingTransactions:    string;
+      clearedBalance:     string;
+      currency: string;
+    }
+    
     var json = this.getBalance()
     json.then( x => {
-      //console.log(x.amount);
-      //var amount = (x.amount).toString();
-      //this.euros = amount.split('.')[0]
-      //this.cents = amount.split('.')[1]
+      let balance: Balance = <Balance>x;
+      var amount = (balance.amount).toString();
+      this.euros = amount.split('.')[0]
+      this.cents = amount.split('.')[1]
     } ) 
   }
 
   toogleMenu() {
-    console.log("Toggle " + this.hideMenu);
     this.hideMenu = !this.hideMenu;
   }
 
   getBalance() {
     return new Promise(resolve => {
       this.http.get('http://localhost:1200/balance').subscribe(data => {
-        //console.log(data);
         resolve(data);
       }, err => {
         console.log(err);
